3640df0aef0e152c79a8ec6b8cdf3fec93f7bf60
unknown <pratikborhade302@gmail.com>
2020-11-24 18:30:57 +0530

    commit_1

---
+++ src/test/java/com/google/maps/ApiExceptionTest.java
@@ -0,0 +1,18 @@
+package com.google.maps;
+
+//import com.google.appengine.repackaged.com.google.rpc.context.SystemParameterContextOrBuilder;
+import com.google.maps.errors.ApiException;
+import com.google.maps.errors.OverDailyLimitException;
+import com.google.maps.errors.OverQueryLimitException;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+
+
+public class ApiExceptionTest {
+    @Test
+    public void name() {
+        System.out.println("run");
+    }
+}
